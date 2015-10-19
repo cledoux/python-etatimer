64838b4681fe37c055f61b0e5c7cb1fee04be64d
Charles LeDoux <charles.a.ledoux@gmail.com>
2015-10-19 16:34:29 -0500

    Finished initial adapting work

--- etatimer.py
+++ etatimer.py
@@ -5,7 +5,7 @@ from __future__ import division, absolute_import, with_statement
 import sys
 import time
 
-from progressbar2 import ETA, AdaptiveETA, Percentage, StopWatch, SimpleProgress, format_updatable
+from progressbar import ProgressBar, ETA, AdaptiveETA, Percentage, SimpleProgress, Timer
 
 
 class EtaTimer(object):
@@ -19,70 +19,34 @@ class EtaTimer(object):
     called and final statistics are reported. The timer can manually be stopped
     by a call to ding() as well.
     """
-    def __init__(self, total, name="", stream=sys.stdout):
+    def __init__(self, total, name="", stream=sys.stderr):
         """
         Arguments:
             total:  Total number of items that will need processing.
             stream:     File descriptor to write to
         """
-        self.stream = stream
-        # The current number of values already processed
-        self.currval = 0
-        # The total number of items to process.
-        # Naming is weird to match expectations of adapted progressbar2 code.
-        self.maxval = total
-        self.start_time = time.time()
-        self.seconds_elapsed = 0
-        self.last_update_time = 0
-        self.poll = 1
-        self.eta_window = 0
-        self._widgets = self._default_widgets()
         self.name = name
-        self.write_update()
-
-    @property
-    def left(self):
-        return self.maxval - self.currval
-
-    def percentage(self):
-        return self.currval * 100.0 / self.maxval
+        self.eta_window = 0
+        self.currval = 0
+        self.total = total
+        self.pbar = ProgressBar(max_value=total, widgets=self._default_widgets(), fd=stream)
 
     @property
     def finished(self):
-        return self.currval == self.maxval
-
-    # backwards compatibility alias
-    def done(self):
-        return self.finished
-
-    def write_update(self):
-        self.fd.write('\r' + self._format_line())
-        self.fd.flush()
-        self.last_update_time = time.time()
+        return self.currval >= self.total
 
     def tick(self, *args):
         """ Finished an item.
         The *args is so this can be used as an arbitrary callback
         """
+        self.pbar.update(self.currval)
         self.currval += 1
-        self.seconds_elapsed = time.time() - self.start_time
         if self.finished:
             self.ding()
-        else:
-            # Only update once a second at most
-            if time.time() - self.last_update_time > self.poll:
-                self.write_update()
 
     def ding(self):
         """ Time's up. Ding! """
-        self.seconds_elapsed = time.time() - self.start_time
-        # Clear current line
-        # If using the ANSI escape sequence is ever an issue, we
-        # can start tracking the length of the last output line
-        # and using \r to put the proper number of spaces over the line.
-        self.fd.write('\r\x1b[K')
-        self.write_update()
-        self.fd.write('\n')
+        self.pbar.finish()
 
     # progressbar2 adapted code
     def _default_widgets(self):
@@ -92,19 +56,8 @@ class EtaTimer(object):
             eta = AdaptiveETA(num_samples=self.eta_window)
         return [
             Percentage(), ' (', SimpleProgress(), ') ',
-            StopWatch(), ' ', eta,
+            Timer(), ' ', eta,
         ]
-
-    def _format_line(self):
-        if self.finished:
-            # Ignore ETA if finished, already including StopWatch
-            result = [format_updatable(widget, self)
-                      for widget in self._widgets[:-2]]
-        else:
-            result = [format_updatable(widget, self)
-                      for widget in self._widgets]
-
-        return ''.join(result)
 
 
 class DummyTimer(object):
